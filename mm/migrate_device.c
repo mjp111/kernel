990535564eafa648463aee5436380b019e966e43
Mika Penttilä <mpenttil@redhat.com>
2025-07-28 20:07:14 +0300

    mm: unified fault and migrate device page paths
    As of this writing, the way device page faulting and migration works
    is not optimal, if you want to do both fault handling and
    migration at once.
    
    Being able to migrate not present pages (or pages mapped with incorrect
    permissions, eg. COW) to the GPU requires doing either of the
    following sequences:
    
    1. hmm_range_fault() - fault in non-present pages with correct permissions, etc.
    2. migrate_vma_*() - migrate the pages
    
    Or:
    
    1. migrate_vma_*() - migrate present pages
    2. If non-present pages detected by migrate_vma_*():
       a) call hmm_range_fault() to fault pages in
       b) call migrate_vma_*() again to migrate now present pages
    
    The problem with the first sequence is that you always have to do two
    page walks even when most of the time the pages are present or zero page
    mappings so the common case takes a performance hit.
    
    The second sequence is better for the common case, but far worse if
    pages aren't present because now you have to walk the page tables three
    times (once to find the page is not present, once so hmm_range_fault()
    can find a non-present page to fault in and once again to setup the
    migration). It also tricky to code correctly.
    
    We should be able to walk the page table once, faulting
    pages in as required and replacing them with migration entries if
    requested.
    
    Add a new flag to HMM APIs, HMM_PFN_REQ_MIGRATE,
    which tells to prepare for migration also during fault handling.
    Also, for the migrate_vma_setup() call paths, a flags, MIGRATE_VMA_FAULT,
    is added to tell to add fault handling to migrate.
    
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Leon Romanovsky <leonro@nvidia.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Balbir Singh <balbirs@nvidia.com>
    
    Suggested-by: Alistair Popple <apopple@nvidia.com>
    Signed-off-by: Mika Penttilä <mpenttil@redhat.com>
    
    inout flags
    
    tests

--- mm/migrate_device.c
+++ mm/migrate_device.c
@@ -18,6 +18,7 @@
 #include <asm/tlbflush.h>
 #include "internal.h"
 
+#if 0
 static int migrate_vma_collect_skip(unsigned long start,
 				    unsigned long end,
 				    struct mm_walk *walk)
@@ -525,7 +526,7 @@ static void migrate_vma_collect(struct migrate_vma *migrate)
 	mmu_notifier_invalidate_range_end(&range);
 	migrate->end = migrate->start + (migrate->npages << PAGE_SHIFT);
 }
-
+#endif
 /*
  * migrate_vma_check_page() - check if page is pinned or not
  * @page: struct page to check
@@ -734,7 +735,17 @@ static void migrate_vma_unmap(struct migrate_vma *migrate)
  */
 int migrate_vma_setup(struct migrate_vma *args)
 {
+	int ret;
 	long nr_pages = (args->end - args->start) >> PAGE_SHIFT;
+	struct hmm_range range = {
+		.notifier = NULL,
+		.start = args->start,
+		.end = args->end,
+		.migrate = args,
+		.hmm_pfns = args->src,
+		.dev_private_owner = args->pgmap_owner,
+		.migrate = args
+	};
 
 	args->start &= PAGE_MASK;
 	args->end &= PAGE_MASK;
@@ -759,17 +770,19 @@ int migrate_vma_setup(struct migrate_vma *args)
 	args->cpages = 0;
 	args->npages = 0;
 
-	migrate_vma_collect(args);
+	if (args->flags & MIGRATE_VMA_FAULT)
+		range.default_flags |= HMM_PFN_REQ_FAULT;
 
-	if (args->cpages)
-		migrate_vma_unmap(args);
+	ret = hmm_range_fault(&range);
+
+	migrate_hmm_range_setup(&range);
 
 	/*
 	 * At this point pages are locked and unmapped, and thus they have
 	 * stable content and can safely be copied to destination memory that
 	 * is allocated by the drivers.
 	 */
-	return 0;
+	return ret;
 
 }
 EXPORT_SYMBOL(migrate_vma_setup);
@@ -1489,3 +1502,63 @@ int migrate_device_coherent_folio(struct folio *folio)
 		return 0;
 	return -EBUSY;
 }
+
+void migrate_hmm_range_setup(struct hmm_range *range)
+{
+
+	struct migrate_vma *migrate = range->migrate;
+
+	if (!migrate)
+		return;
+
+	migrate->npages = (migrate->end - migrate->start) >> PAGE_SHIFT;
+	migrate->cpages = 0;
+
+	for (unsigned long i = 0; i < migrate->npages; i++) {
+
+		unsigned long pfn = range->hmm_pfns[i];
+		pfn &= ~HMM_PFN_INOUT_FLAGS;
+
+		/*
+		 *
+		 *  Don't do migration if valid and migrate flags are not both set.
+		 *
+		 */
+		if ((pfn & (HMM_PFN_VALID | HMM_PFN_MIGRATE)) !=
+		    (HMM_PFN_VALID | HMM_PFN_MIGRATE)) {
+			migrate->src[i] = 0;
+			migrate->dst[i] = 0;
+			continue;
+		}
+
+		migrate->cpages++;
+
+		/*
+		 *
+		 * The zero page is encoded in a special way, valid and migrate is
+		 * set, and pfn part is zero. Encode specially for migrate also.
+		 *
+		 */
+		if (pfn == (HMM_PFN_VALID|HMM_PFN_MIGRATE)) {
+			migrate->src[i] = MIGRATE_PFN_MIGRATE;
+			migrate->dst[i] = 0;
+			continue;
+		}
+		if (pfn == (HMM_PFN_VALID|HMM_PFN_MIGRATE|HMM_PFN_COMPOUND)) {
+			migrate->src[i] = MIGRATE_PFN_MIGRATE|MIGRATE_PFN_COMPOUND;
+			migrate->dst[i] = 0;
+			continue;
+		}
+
+		migrate->src[i] = migrate_pfn(page_to_pfn(hmm_pfn_to_page(pfn)))
+			| MIGRATE_PFN_MIGRATE;
+		migrate->src[i] |= (pfn & HMM_PFN_WRITE) ? MIGRATE_PFN_WRITE : 0;
+		migrate->src[i] |= (pfn & HMM_PFN_COMPOUND) ? MIGRATE_PFN_COMPOUND : 0;
+		migrate->dst[i] = 0;
+	}
+
+	if (migrate->cpages)
+		migrate_vma_unmap(migrate);
+
+}
+EXPORT_SYMBOL(migrate_hmm_range_setup);
