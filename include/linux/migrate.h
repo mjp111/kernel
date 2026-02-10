6e344b112ac2837afcc82ed6c166d626c244e961
Mika Penttilä <mpenttil@redhat.com>
2026-02-10 10:21:16 +0200

    mm/hmm: do the plumbing for HMM to participate in migration
    Do the preparations in hmm_range_fault() and pagewalk callbacks to
    do the "collecting" part of migration, needed for migration
    on fault.
    
    These steps include locking for pmd/pte if migrating, capturing
    the vma for further migrate actions, and calling the
    still dummy hmm_vma_handle_migrate_prepare_pmd() and
    hmm_vma_handle_migrate_prepare()  functions in the pagewalk.
    
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Leon Romanovsky <leonro@nvidia.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Balbir Singh <balbirs@nvidia.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Suggested-by: Alistair Popple <apopple@nvidia.com>
    Signed-off-by: Mika Penttilä <mpenttil@redhat.com>

--- include/linux/migrate.h
+++ include/linux/migrate.h
@@ -104,6 +104,15 @@ static inline void softleaf_entry_wait_on_locked(softleaf_t entry, spinlock_t *p
 	WARN_ON_ONCE(1);
 
 	spin_unlock(ptl);
+
+enum migrate_vma_info {
+	MIGRATE_VMA_SELECT_NONE = 0,
+	MIGRATE_VMA_SELECT_COMPOUND = MIGRATE_VMA_SELECT_NONE,
+};
+
+static inline enum migrate_vma_info hmm_select_migrate(struct hmm_range *range)
+{
+	return MIGRATE_VMA_SELECT_NONE;
 }
 
 #endif /* CONFIG_MIGRATION */
@@ -149,7 +158,7 @@ static inline unsigned long migrate_pfn(unsigned long pfn)
 	return (pfn << MIGRATE_PFN_SHIFT) | MIGRATE_PFN_VALID;
 }
 
-enum migrate_vma_direction {
+enum migrate_vma_info {
 	MIGRATE_VMA_SELECT_SYSTEM = 1 << 0,
 	MIGRATE_VMA_SELECT_DEVICE_PRIVATE = 1 << 1,
 	MIGRATE_VMA_SELECT_DEVICE_COHERENT = 1 << 2,
@@ -190,6 +199,12 @@ struct migrate_vma {
 	 */
 	struct page		*fault_page;
 };
+
+// TODO: enable migration
+static inline enum migrate_vma_info hmm_select_migrate(struct hmm_range *range)
+{
+	return 0;
+}
 
 int migrate_vma_setup(struct migrate_vma *args);
 void migrate_vma_pages(struct migrate_vma *migrate);
